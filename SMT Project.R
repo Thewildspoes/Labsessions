32a8a27e20e5e6fe0c6f88b5f65564f45bbb855b
Spoes <deruyterdewildtiris@gmail.com>
2020-02-23 17:56:47 +0100

    Project File

---
+++ SMT Project.R
@@ -0,0 +1,22 @@
+# hier zetten we de working directory vast. Dit moet verandert worden als 
+# er een andere laptop gebruikt wordt. 
+setwd("/Users/irisderuyterdewildt/Desktop/EUR/SMT/Labsessions")
+
+# Hier lees je de file in met de data van de enquete. Dit moet csv2 zijn
+# omdat het een ; separated value lijst is en niet komma. 
+dsCase <- read.csv2(file = "Data/SMT1920casus01.csv",
+                   stringsAsFactors = FALSE)
+
+#Hier wordt het psych pakket eenmalig geinstalleerd. Door de installatie
+# van dit pakket kan men gemakkelijker functies berekenen die niet automatisch
+# in R studio zitten.
+install.packages("psych", dependencies = TRUE)
+library(psych)
+
+dsCase["PreferAirplane"]
+
+mean(dsCase$PreferAirplane, na.rm = TRUE)
+dsCase$PreferAirplane[is.na(dsCase$PreferAirplane)] <-
+  round(mean(dsCase$PreferAirplane, na.rm=TRUE))
+
+
