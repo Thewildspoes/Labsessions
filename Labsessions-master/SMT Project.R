7b748e8399b2b30cea6e36720baa5b0d130d22d5
Amani <amani.berkhof@hotmail.com>
2020-02-27 13:00:41 +0100

    ggplot

--- Labsessions-master/SMT Project.R
+++ Labsessions-master/SMT Project.R
@@ -6,13 +6,13 @@
 # hier neergezet om zo allemaal in hetzelfde bestand te kunnen werken. H
 # Vergeet niet te kijken of je de juiste WD aan het staan. Een WD van iemand anders
 # kan je uitzetten door voor "SetWD" een # te zetten. 
-setwd("/Users/irisderuyterdewildt/Desktop/EUR/SMT/Labsessions")
+#setwd("/Users/irisderuyterdewildt/Desktop/EUR/SMT/Labsessions")
 
 setwd("/Users/amaniberkhof/Documents/Labsessions")
 
-setwd("/Users/luliheerkens/Documents/Bedrijfskunde (BA)/practicum S&T/Data")
+#setwd("/Users/luliheerkens/Documents/Bedrijfskunde (BA)/practicum S&T/Data")
 
-setwd("C:/Users/Barbara/Documents")
+#setwd("C:/Users/Barbara/Documents")
 
 
 # Hier lees je de file in met de data van de enquete. Dit moet csv2 zijn
@@ -194,8 +194,17 @@ describe(dsCase$SchipholCar)
 # Analyse Paarsgewijze Samenhangen
 #------------------------------------------------------------------------------------
 
-
-
+#Kwantitatief
+---------------------------------------------------------------------------------
+  
+#ImportComfort en RateAirplane 
+-------------------------------------------------------------------------------------
+# Base plot
+  ggplot(dsCase, aes(x=rateTrain, y=rateAirplane)) +
+  geom_point(col="blue") + ylab("Likelihood to take the airplane (rateAirplane)") +
+  xlab("Comfort is the most import element (rateTrain)")
+  
+  
 #------------------------------------------------------------------------------------
 # Doorkruisendheden en Interactie
 #------------------------------------------------------------------------------------
